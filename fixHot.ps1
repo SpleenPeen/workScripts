fc8d6269dd43810f2314a7b353573130c8cd515c
Mateusz <mateusz.wosik2002@gmail.com>
2025-08-21 11:36:33 +0100

    idk why i thought the names would be the same
    fixed now 👍

--- fixHot.ps1
+++ fixHot.ps1
@@ -1,6 +1,17 @@
-pnputil /delete-driver oem42.inf /uninstall /force
-pnputil /delete-driver oem43.inf /uninstall /force
-pnputil /delete-driver oem45.inf /uninstall /force
+#find driver name of hotkey service
+$all = Get-CimInstance -ClassName Win32_PnPSignedDriver;
+$name = "";
+foreach ($curDev in $all) 
+{ 
+ if ($curDev.DeviceName -like "HP LAN/WLAN/WWAN Switching and Hotkey Service") 
+ { 
+  $name =  $curDev.InfName; 
+ } 
+}
 
+#delete driver (if found)
+if ($name -ne "") { pnputil /delete-driver $name /uninstall /force; }
+
+#add stable driver from folder
 $driver = $PSScriptRoot + "/Driver/HpqKbSoftwareCompnent.inf"
 pnputil /add-driver $driver /install
