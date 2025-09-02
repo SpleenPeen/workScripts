b9bfd2ed26c4551f693d6df29cb3e96231f43ef8
Mateusz <mateusz.wosik2002@gmail.com>
2025-09-02 11:03:31 +0100

    should fix the 'accessed by another process' errors when attempting to delete the folder

--- searchwin10.ps1
+++ searchwin10.ps1
@@ -1,6 +1,6 @@
 	# DELETE SEARCH FOLDER
 $sFolder = "C:/Users/Administrator/AppData/Local/Packages/Microsoft.Windows.Search_cw5n1h2txyewy"
-if(test-path -path $sFolder) {remove-item -path $sFolder -recurse -force}
+if(test-path -path $sFolder) {get-childitem $sFolder -recurse -force | remove-item -recurse -force; remove-item $sFolder -recurse -force}
 
 	# DELETE SEARCH KEY
 $sKey = "HKCU:/SOFTWARE/Microsoft/Windows/CurrentVersion/Search"
@@ -8,9 +8,6 @@ if(test-path -path $sKey) {remove-item -path $sKey -recurse -force}
 
 	# DOWNLOAD SEARCH PACKAGE
 add-appxpackage "C:/Windows/SystemApps/Microsoft.Windows.Search_cw5n1h2txyewy/Appxmanifest.xml" -DisableDevelopmentMode -Register
-
-	# RESET POLICY
-set-executionpolicy default
 
 	# REBOOT
 restart-computer
